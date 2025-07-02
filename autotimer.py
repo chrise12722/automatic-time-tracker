215142f6c377e61a05184986a2d1d76b43628a04
chrise12722 <159836685+chrise12722@users.noreply.github.com>
2025-07-02 12:04:26 -0400

    feat: add timer function Made the code into a recursive function that tracks the start time of an active tab/app, and tracks the end time when the app/tab is different, getting the total elapsed time (time spent on tab/app)

--- autotimer.py
+++ autotimer.py
@@ -4,10 +4,14 @@ import time
 from urllib.parse import urlparse
 from firefox_grab_url import get_current_browser_url
 
-while True:
+def Track_App(start_time, last_active_app):
   active_window = pwc.getActiveWindow()
   if active_window:
     active_app = active_window.getAppName()
+    #Initial call to function
+    if start_time == 0 and last_active_app == "None":
+      start_time = time.time()
+      Track_App(start_time, active_app)
     if active_app == 'Google Chrome' or active_app == 'Safari' or active_app == 'firefox':
       match active_app:
         case 'Google Chrome':
@@ -23,9 +27,30 @@ while True:
         case 'firefox':
           tab = get_current_browser_url()
           tab = urlparse(tab).netloc
-      print(tab)
+      #Is tab the same as last time function ran
+      if tab == last_active_app:
+        time.sleep(10)
+        Track_App(start_time, tab)
+      else:
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        # Add entry to data.json
+        start_time = time.time()
+        time.sleep(10)
+        Track_App(start_time, tab)
     else:
-      print(active_app)
+      #Is app the same as last time function ran
+      if active_app == last_active_app:
+        time.sleep(10)
+        Track_App(start_time, active_app)
+      else:
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        # Add entry to data.json
+        start_time = time.time()
+        time.sleep(10)
+        Track_App(start_time, active_app)
   
-  time.sleep(10)
+  while True:
+    Track_App(0, "None")
 
